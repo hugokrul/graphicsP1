09afe8e3bb51388ae531c822115cd9002dd35fd1
KoenVermeulenNL <84975944+KoenVermeulenNL@users.noreply.github.com>
2023-06-02 01:38:58 +0200

    spotlight

--- raytracer/Light.cs
+++ raytracer/Light.cs
@@ -20,4 +20,15 @@ namespace INFOGR2023Template
             color = c;
         }
     }
+    public class SpotLight : Light
+    {
+        public Vector3 Direction;
+        public float maxAngle;
+
+        public SpotLight(Vector3 p, int i, Vector3 c, Vector3 Direction, float maxAngle) : base(p, i, c)
+        {
+            this.Direction = Direction;
+            this.maxAngle = maxAngle;
+        }
+    }
 }
